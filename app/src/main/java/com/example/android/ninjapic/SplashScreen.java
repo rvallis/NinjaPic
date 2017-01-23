53e23ceb083059d01fda3d533464a26663a73d71
Robin Vallis <rvallis>
2017-01-23 15:09:38 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/ninjapic/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.android.ninjapic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.ninjapic.MainActivity;
+import com.example.android.ninjapic.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
